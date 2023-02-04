df19961ea3e4a7e6910d5c967a1aebb7c597d28d
Andy Teijelo <ateijelo@gmail.com>
2023-02-04 12:53:16 -0500

    Make the app configurable via config file and env vars

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,17 @@ use reqwest::Client;
 #[derive(Parser, Debug)]
 #[command()]
 struct Args {
-    #[arg(short, long, default_value = "default")]
+    #[arg(short, long, env = "JENKINS_CLI_PROFILE", default_value = "default")]
     profile: String,
+
+    #[arg(long, default_value_t = false)]
+    show_config_path: bool,
+
+    #[arg(long, default_value_t = false)]
+    show_config: bool,
+
+    #[arg(short, long)]
+    config_path: Option<String>,
 }
 
 mod profile;
@@ -13,8 +22,24 @@ mod profile;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     use profile::Profile;
+
     let args = Args::parse();
-    let profile = Profile::new(&args.profile)?;
+
+    if args.show_config_path {
+        println!(
+            "{}",
+            Profile::config_path(&args.config_path, &args.profile)?
+                .to_str()
+                .unwrap_or("")
+        );
+        std::process::exit(0);
+    }
+
+    let profile = Profile::new(&args.config_path, &args.profile)?;
+
+    if args.show_config {
+        println!("{}", profile);
+    }
 
     let client = Client::new();
     let resp = client
--- src/profile.rs
+++ src/profile.rs
@@ -1,26 +1,70 @@
 use config::Config;
+use directories::ProjectDirs;
+use std::{collections::HashMap, fmt::Display, path::PathBuf};
 
 pub struct Profile {
     pub username: String,
     pub password: String,
     pub url: String,
+    pub aliases: HashMap<String, String>,
 }
 
-impl Profile {
-    pub fn new(profile: &str) -> Result<Self, Box<dyn std::error::Error>> {
-        let config = Config::builder()
-            .add_source(config::File::with_name("jenkins-cli.toml"))
-            // .add_source(config::Environment::with_prefix("JENKINS_CLI"))
-            .build()?;
+type Err = Box<dyn std::error::Error>;
 
-        let url: String = config.get(&format!("{}.url", profile))?;
-        let username: String = config.get(&format!("{}.username", profile))?;
-        let password: String = config.get(&format!("{}.password", profile))?;
+impl Profile {
+    pub fn config_path(path: &Option<String>, profile: &str) -> Result<PathBuf, Err> {
+        if let Some(p) = path {
+            // if a config file path is provided, we use it
+            return Ok(PathBuf::from(p));
+        }
+        if let Some(dirs) = ProjectDirs::from("", "", "jenkins-cli") {
+            // if no config file was provided, but a standard profile exists
+            // then we use that
+            return Ok(dirs.config_dir().join(format!("{}.toml", profile)));
+        }
+        Err("No config path provided, and no standard config directory found".into())
+    }
+
+    pub fn new(config_path: &Option<String>, profile: &str) -> Result<Self, Err> {
+        let mut builder =
+            Config::builder().add_source(config::Environment::with_prefix("JENKINS_CLI"));
+
+        let cfg_path = Self::config_path(config_path, profile)?;
+        // println!("cfg_path: {:?}", cfg_path);
+
+        if config_path.is_none() {
+            if cfg_path.exists() {
+                builder = builder.add_source(config::File::from(cfg_path));
+            }
+        } else {
+            builder = builder.add_source(config::File::from(cfg_path));
+        }
+
+        let config = builder.build()?;
+
+        let url: String = config.get("url")?;
+        let username: String = config.get("username")?;
+        let password: String = config.get("password")?;
+        let aliases = config
+            .get_table("aliases")?
+            .iter()
+            .map(|(k, v)| (k.clone(), v.to_string()))
+            .collect();
 
         Ok(Self {
             url,
             username,
             password,
+            aliases,
         })
+    }
+}
+
+impl Display for Profile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "url: {}", self.url)?;
+        writeln!(f, "username: {}", self.username)?;
+        writeln!(f, "password: {}", "*".repeat(self.password.len()))?;
+        write!(f, "aliases: {:?}", self.aliases)
     }
 }
